3dde9edba09d526f9c04edd1b8ef435e8464730f
Suraj Maity <surajmaity981@gmail.com>
2025-12-02 10:22:13 +0530

    draft: few changes ( work pending )

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,9 @@ import { BrowserRouter } from "react-router-dom";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 
-const stripePromise = loadStripe("<ENTER STRIPE Publishable key>");
+const stripePromise = loadStripe(
+  "pk_test_51OI2a2SBsZzmapIXJukuvYNR4awDy4lgT007nch7segINw6uX2ewHSyMcGU4gP59xIEXY7Ns0yu35s8g2Bt2VGfs00QUN5sI3I"
+);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
